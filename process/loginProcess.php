80570c02b3fc2c802a28e99f8a069beaa413c63c
arielpirie <200710784@students.uajy.ac.id>
2022-10-15 00:53:47 +0700

    Commit loginProcess dan registerProcess (Unfinished)

---
+++ process/loginProcess.php
@@ -0,0 +1,47 @@
+<?php
+    // ini buat ngecek tombol yang namenya 'register' sudah di pencet atau belum
+    // $_POST itu method di formnya
+    if(isset($_POST['login'])){
+
+        include('../db.php'); // untuk mengoneksikan dengan databas dengan memanggil file db.php
+        //tampung nilai yang ada di from ke variable
+        // sesuaikan variabel name yang ada di registerPage.php disetiap input
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        // Melakukan insert ke databse dengan query dibawah ini
+        $query = mysqli_query($con, "SELECT * FROM user WHERE email = '$email'") or
+die(mysqli_error($con));
+        // ini buat ngecek kalo misalnya hasil dari querynya == 0 ato ga ketemu -> emailnya tdk ditemukan
+        if(mysqli_num_rows($query) == 0){
+            echo
+            '<script>
+            alert("Email not found!"); window.location = "../userPage/loginPage.php"
+            </script>';
+        }else{
+            $user = mysqli_fetch_assoc($query);
+            if(password_verify($password, $user['password'])){
+                // session adalah variabel global sementara yang disimpen di server
+                // buat mulai sessionnya pake session_start()
+                session_start();
+                //isLogin ini temp variable yang gunanya buat ngecek nanti apakah sdh login ato belum
+                $_SESSION['isLogin'] = true;
+                $_SESSION['user'] = $user;
+                echo
+                    '<script>
+                    alert("Login Success"); window.location = "../page/dashboardPage.php"
+                    </script>';
+            }else {
+                echo
+                    '<script>
+                    alert("Email or Password Invalid");
+                    window.location = "../userPage/loginPage.php"
+                    </script>';
+            }
+        }
+    }else{
+        echo
+            '<script>
+            window.history.back()
+            </script>';
+    }
+?>
--- process/registerProcess.php
+++ process/registerProcess.php
@@ -11,52 +11,45 @@
         $nama_user = $_POST['nama_user'];
         $nama_foto = $_POST['nama_foto'];
 
-         // Melakukan insert ke databse dengan query dibawah ini
-                        $query = mysqli_query($con,
-                        "INSERT INTO users(email, password, nama_user, nama_foto) 
-                            VALUES
-                        ('$email', '$password', '$nama_user', '$nama_foto')")
-                            or die(mysqli_error($con)); // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
-        
-        // $query_p = mysqli_query($con, "SELECT * FROM user WHERE phonenum = '$phonenum'") or
+        // $query_p = mysqli_query($con, "SELECT * FROM users WHERE phonenum = '$phonenum'") or
         //         die(mysqli_error($con));
         // $query_e = mysqli_query($con, "SELECT * FROM users WHERE email = '$email'") or
         //         die(mysqli_error($con));
 
-        //         if(mysqli_num_rows($query_p) == 0){
+                // if(mysqli_num_rows($query_p) == 0){
                     
-        //             if(mysqli_num_rows($query_e) == 0){
-        //                 // Melakukan insert ke databse dengan query dibawah ini
-        //                 $query = mysqli_query($con,
-        //                 "INSERT INTO users(email, password, name, phonenum, membership, job) 
-        //                     VALUES
-        //                 ('$email', '$password', '$name', '$phonenum', '$membership', '$job')")
-        //                     or die(mysqli_error($con)); // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
-        //                 if($query){
-        //                     echo
-        //                         '<script>
-        //                         alert("Register Success"); 
-        //                         window.location = "../index.php"
-        //                         </script>';
-        //                 }
-        //                 else{
-        //                     echo
-        //                         '<script>
-        //                         alert("Register Failed");
-        //                         </script>';
-        //                 }
-        //             }else{
-        //                 echo
-        //                 '<script>
-        //                 alert("Email must be unique!"); window.location = "../page/registerPage.php"
-        //                 </script>';
-        //             }
-        //         }else{
-        //             echo
-        //             '<script>
-        //             alert("Phonenum must be unique!"); window.location = "../page/registerPage.php"
-        //             </script>';
-        //         }
+                    // if(mysqli_num_rows($query_e) == 0){
+                        // Melakukan insert ke databse dengan query dibawah ini
+                        $query = mysqli_query($con,
+                        "INSERT INTO user(email, password, nama_user, nama_foto) 
+                            VALUES
+                        ('$email', '$password', '$nama_user', '$nama_foto')")
+                            or die(mysqli_error($con)); // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
+                        if($query){
+                            echo
+                                '<script>
+                                alert("Register Success"); 
+                                window.location = "../index.php"
+                                </script>';
+                        }
+                        else{
+                            echo
+                                '<script>
+                                alert("Register Failed");
+                                </script>';
+                        }
+                    // }else{
+                    //     echo
+                    //     '<script>
+                    //     alert("Email must be unique!"); window.location = "../page/registerPage.php"
+                    //     </script>';
+                    // }
+                // }else{
+                //     echo
+                //     '<script>
+                //     alert("Phonenum must be unique!"); window.location = "../page/registerPage.php"
+                //     </script>';
+                // }
   
     }else{
         echo
