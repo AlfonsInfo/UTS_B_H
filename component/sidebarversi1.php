160c8c6460d1288d230dadd1dbe3061794f7952a
Alfonsdev <itsmealfons@gmail.com>
2022-10-15 15:48:22 +0700

    Sidebar + tabel peminjaman

--- component/sidebarversi1.php
+++ component/sidebarversi1.php
@@ -16,8 +16,11 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-
 EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../style.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
@@ -66,9 +69,14 @@ EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="a
                 <h2 class="text-light text-center pt-2">LIBRARY</h2>
                 <hr>
                 <div class="menu">
+                    <div>
+                        <img src="" alt="foto profile">
+                        <a href=""></a>
+                    </div>
+                    <?var_dump($_SESSION)?>
                     <div class="content-menu">
-                        <i class="fa fa-dashboard"></i>
-                        <a href="" style="font-weight:600">PEMINJAMAN BUKU</a>
+                        <i class="fa-solid fa-hand-holding"></i>
+                        <a href="../page/" style="font-weight:600">PEMINJAMAN BUKU</a>
                     </div>
                     <div class="content-menu ">
                         <i class="fa fa-film"></i>
