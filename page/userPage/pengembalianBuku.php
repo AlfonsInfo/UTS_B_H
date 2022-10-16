f08762937238ea0a53639629699de2882b384e8d
Alfonsdev <itsmealfons@gmail.com>
2022-10-16 08:22:31 +0700

    Tabel pengembalian buku dasar...

--- page/userPage/pengembalianBuku.php
+++ page/userPage/pengembalianBuku.php
@@ -8,44 +8,44 @@
 solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 
 0.19);">
     <div class="body d-flex justify-content-between">
-        <h4>LIST BUKU</h4>
+        <h4>LIST BUKU YANG DIPINJAM </h4>
     </div>
     <hr>
     <table class="table ">
         <thead>
             <tr>
-                <th scope="col">No</th>
-                <th scope="col">Judul</th>
+                <th scope="col">Nama Buku</th>
                 <th scope="col">Sampul</th>
-                <th scope="col">Tersedia</th>
-                <th scope="col"></th>
+                <th scope="col">Status</th>
+                <th scope="col">Tanggal Pengembalian</th>
+                <th scope="col">Aksi</th>
             </tr>
         </thead>
         <tbody>
-            <?php $query = mysqli_query($con, "SELECT * FROM buku") or die(mysqli_error($con));
+            <?php 
+                $email = $_SESSION['user']['email'];
+                $query = mysqli_query($con, "SELECT * FROM peminjaman inner join buku  on peminjaman.id_buku = buku.id_buku WHERE email_user = '$email' ") or die(mysqli_error($con));
                 if (mysqli_num_rows($query) == 0) {
                     echo '<tr> <td colspan="7"> Tidak ada data </td> </tr>';
                 }else{
-                    $no = 1;
                 while($data = mysqli_fetch_assoc($query)){
+                    // var_dump($data);die;
                 echo'
                     <tr>
-                    <th scope="row">'.$no.'</th>
                     <td>'.$data['nama_buku'].'</td>
-                    <td><img src="url().img/assets/'.$data['nama_sampul'].' alt =gambarsampul width=300px></td>
-                    <td>'.$data['jumlah_tersedia'].'</td>
-                    <td>
-                    <a href="../../process/pinjamProcess.php?id='.$data['id_buku'].'&email='.$_SESSION['user']['email'].'" 
+                    <td><img src=../../img/assets/'.$data['nama_sampul'].' alt =gambarsampul width=300px></td>
+            <td>'.$data['status'].'</td>
+            <td>'.$data['tanggal_pengembalian'].'</td>
+            <td> 
+            <a href="../../process/pengembalianProcess.php?id='.$data['id_buku'].'&email='.$_SESSION['user']['email'].'" 
                         onClick="return confirm ( \'Are you sure want to delete this 
-                        data?\')"> <i style="color: green" class="fa fa-book fa-lg"> Pinjam</i>
+                        data?\')"> <i style="color: green" class="fa fa-book fa-lg"> Kembalikan</i>
                         </a>
-                    </td>
-                    </td>
-                    </tr>';
-                    $no++;
-                }
-                }
-?>
+            </td>            
+            </tr>';
+            }
+            }
+            ?>
         </tbody>
     </table>
 </div>
