b7f8a42e4d000d3f7c7a871f5aa680726fd364d2
Alfonsdev <itsmealfons@gmail.com>
2022-10-16 08:31:13 +0700

    uncomment query untuk insert data peminjaman

--- process/pinjamProcess.php
+++ process/pinjamProcess.php
@@ -9,15 +9,17 @@ if (isset($_GET['id']) && isset($_GET['email'])) {
     /* Menjumlahkan waktu dari awal dengan penambahan waktu yang telah ditentukan.*/
     $tanggal_kembali = date('Y-m-d', strtotime('+7 days', strtotime($tanggal_pinjam)));
     $status = "Pinjam";
-    // $query = mysqli_query($con, "INSERT INTO peminjaman(email_user, id_buku, tanggal_pengembalian, status) VALUES('$email', '$id', '$tanggal_kembali', '$status')") or die(mysqli_error($con));
-
+    
     // if ($query) {
         $queryKurangJumlah = mysqli_query($con, "SELECT * FROM buku WHERE id_buku = '$id'") or die(mysqli_error($con));
         $data = mysqli_fetch_assoc($queryKurangJumlah);
         $jumlah = $data['jumlah_tersedia'] - 1;
-    
+        
         if($jumlah!=-1){
+            $query = mysqli_query($con, "INSERT INTO peminjaman(email_user, id_buku, tanggal_pengembalian, status) VALUES('$email', '$id', '$tanggal_kembali', '$status')") or die(mysqli_error($con));
             $query1 = mysqli_query($con, "UPDATE buku SET jumlah_tersedia = '$jumlah' WHERE id_buku = '$id'") or die(mysqli_error($con));
+            // $query2 = mysqli_query($con, "INSERT INTO peminjaman(id_peminjaman, email_user, tanggal_pengembalian, status) values
+                    // ('',4)");
             echo
             '<script>
                     alert("Berhasil Pinjam Buku, Tekan OK maka akan di eksekusi4"); window.location = "../page/crudPage/readBukuPage.php"
